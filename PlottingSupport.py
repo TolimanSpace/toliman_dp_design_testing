1a5da5bcaa20c024f94cd2af5ba7194a15301f4c
Clan2098 <134997657+CLangford2098@users.noreply.github.com>
2023-10-26 17:09:15 +1100

    Fixes plotting function

--- PlottingSupport.py
+++ PlottingSupport.py
@@ -5,9 +5,13 @@ import jax.numpy as np
 class Plotting:
     @staticmethod
     def print_colormap(
-        array: np.ndarray, colormap: str = "viridis", colorbar: bool = False
+        array: np.ndarray,
+        title: str = "",
+        colormap: str = "viridis",
+        colorbar: bool = False,
     ):
         plt.imshow(array, cmap=colormap)
         if colorbar:
             plt.colorbar()
+        plt.title(title)
         plt.show()
